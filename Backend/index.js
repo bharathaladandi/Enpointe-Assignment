aa2ee818c3fcb71918502140c84e207b7145017d
bharathaladandi <bharathaladandi@gmail.com>
2023-05-25 12:32:10 +0530

    Basic Structure Added

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+
+    console.log(`Listen on port ${PORT}`);
+})
