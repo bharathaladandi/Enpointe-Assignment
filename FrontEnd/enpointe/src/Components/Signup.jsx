3702bb828afed5bc733fd1faeb9c8fc520ee9cb8
bharathaladandi <bharathaladandi@gmail.com>
2023-05-25 13:19:22 +0530

    Signup page Added and working correctly

---
+++ FrontEnd/enpointe/src/Components/Signup.jsx
@@ -0,0 +1,138 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'; 
+
+import { useNavigate } from "react-router-dom";
+
+export const Signup = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+
+
+  const [showPassword, setShowPassword] = useState(false);
+
+
+
+  const handleSubmit = (e) => {
+
+
+
+    const payload = {
+      email, password, firstName, lastName,
+  }
+ 
+  fetch("http://localhost:8080/users/signup", {
+      method : "POST",
+      body : JSON.stringify(payload),
+      headers : {
+          'Content-Type': 'application/json'
+      }
+  })
+  .then((res) => res.json())
+  // .then((res) => console.log(res))
+  .catch((err) => console.log(err))
+}
+
+  
+
+  return (
+    <Flex
+    mt={50}
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Sign up
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool features ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input name='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input name='lastName' value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" />
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input name='email' value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input name='password' value={password} onChange={(e) => setPassword(e.target.value)} type={showPassword ? 'text' : 'password'} />
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+              onClick={handleSubmit}
+                loadingText="Submitting"
+                size="lg"
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={'center'}>
+                Already a user? <Link to='/login' color={'blue.400'}>Login</Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
+
--- FrontEnd/enpointe/src/Routes/AllRoutes.jsx
+++ FrontEnd/enpointe/src/Routes/AllRoutes.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import { Homepage } from '../Components/Homepage';
 import { Login } from '../Components/Login';
 import { UserTransations } from '../Components/UserTransations';
+import { Signup } from '../Components/Signup';
 
 export const AllRoutes = () => {
     return (
         <div>
             <Routes>
                 <Route path='/homepage' element={<Homepage />}> </Route>
-                <Route path='/' element={<Login />}></Route>
+                <Route path='/login' element={<Login />}></Route>
+                <Route path='/' element={<Signup />}></Route>
                 <Route path='homepage/:user_id' element={<UserTransations />}></Route>
             </Routes>
         </div>
