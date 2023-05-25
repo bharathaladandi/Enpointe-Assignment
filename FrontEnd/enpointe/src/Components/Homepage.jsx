545f6016620d3b4522f6957a4d7c4a04d92cf0a9
bharathaladandi <bharathaladandi@gmail.com>
2023-05-25 14:23:57 +0530

    data fatched Successfully

--- FrontEnd/enpointe/src/Components/Homepage.jsx
+++ FrontEnd/enpointe/src/Components/Homepage.jsx
@@ -1,7 +1,56 @@
 import React from 'react'
+import { useEffect } from "react"
+import { useState } from "react";
+import axios from "axios";
+
 
 export const Homepage = () => {
+
+
+  // const [loading,setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [data, setdata] = useState("")
+
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/users").then((res) => {
+      console.log(res.data);
+      setdata(res.data);
+    })
+  }, []);
+
   return (
-    <div>Homepage</div>
+    <div>
+      <div>
+        <h1>Account Details </h1>
+        {/* {
+                loading && "Loading....."
+            }
+            {
+                error && "something went wrong...."
+            } */}
+        {
+          data && data.length > 0 && data.map((note) => {
+            return (
+              <div>
+                <p>Account No : {note._id}</p>
+                <p>Email Address : {note.email}</p>
+                <p>firstName : {note.firstName}</p>
+                <p>lastName : {note.lastName}</p>
+                <hr />
+              </div>
+            )
+          })
+        }
+
+      </div>
+
+    </div>
   )
+
+
+
 }
+
+
+
--- FrontEnd/enpointe/src/Components/Login.jsx
+++ FrontEnd/enpointe/src/Components/Login.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Image,
+} from '@chakra-ui/react';
 
 export const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (e) => {
+
+    const payload = {
+      email, password
+    };
+
+
+    fetch("http://localhost:8080/users/login", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) =>{
+        res.json()
+        console.log("Login");
+      })
+      // .then((res) => console.log(res))
+      .catch((err) => console.log(err))
+  };
+
+
+  
   return (
-    <div>Login</div>
-  )
+    <Stack style={{ backgroundColor:'#f3f4f6', marginTop:'60px'}} minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+      <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Stack spacing={4} w={'full'} maxW={'md'}>
+          <Heading fontSize={'2xl'}>Login to your account</Heading>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input name='email' value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Password</FormLabel>
+            <Input name='password' value={password} onChange={(e) => setPassword(e.target.value)} type={showPassword ? 'text' : 'password'} />
+          </FormControl>
+          <Stack spacing={6}>
+            <Stack
+              direction={{ base: 'column', sm: 'row' }}
+              align={'start'}
+              justify={'space-between'}>
+              <Checkbox>Remember me</Checkbox>
+              <Link color={'blue.500'}>Forgot password?</Link>
+            </Stack>
+            <Button
+              onClick={handleSubmit}
+              colorScheme={'blue'} variant={'solid'}>
+              Login
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={'Login Image'}
+          objectFit={'cover'}
+          src={
+            'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+          }
+        />
+      </Flex>
+    </Stack>
+  );
 }
+
